687e672ca17acea16b75360d985fffe2d9b7eced
宣源 <xuanyuan.lb@alibaba-inc.com>
2023-12-28 13:32:58 +0800

    cluster model path

--- scripts/!adetailer.py
+++ scripts/!adetailer.py
@@ -55,6 +55,8 @@ from modules.shared import cmd_opts, opts, state
 
 no_huggingface = getattr(cmd_opts, "ad_no_huggingface", False)
 adetailer_dir = Path(models_path, "adetailer")
+if shared.cmd_opts.just_ui:
+    adetailer_dir = Path(os.path.dirname(shared.cmd_opts.data_dir), "models", "adetailer")
 extra_models_dir = shared.opts.data.get("ad_extra_models_dir", "")
 model_mapping = get_models(
     adetailer_dir, extra_dir=extra_models_dir, huggingface=not no_huggingface
